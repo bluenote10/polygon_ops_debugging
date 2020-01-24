d1fc1e7966ad8eb594ae972ec7eeb83688cee6b7
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-01-24 22:18:53 +0100

    updated rust snippets

--- rust_snippets/snippets.rs
+++ rust_snippets/snippets.rs
@@ -1,3 +1,10 @@
 // To write GeoJSON with plain serde:
+use geojson::{GeoJson, Feature, FeatureCollection, Value, Geometry};
+
+let output_geojson = GeoJson::FeatureCollection(FeatureCollection {
+  bbox: None,
+  features: output_features,
+  foreign_members: None,
+});
 let f = File::create(filename).expect("Unable to create json file.");
 serde_json::to_writer_pretty(f, &output_geojson).expect("Unable to write json file.");
