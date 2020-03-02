5d25cfe8e848163c18200bfd05fbe087011742b7
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-03-02 22:03:44 +0100

    more benchmark results

--- rust_snippets/benchmark.sh
+++ rust_snippets/benchmark.sh
@@ -2,7 +2,7 @@
 
 for i in {1..20}
 do
-  #cargo bench
+  cargo bench --bench benchmark
   #cargo run --bin naive_benchmark --release
-  target/release/naive_benchmark
+  #target/release/naive_benchmark
 done
